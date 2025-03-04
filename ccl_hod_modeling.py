a44c4751047d0734fc517ac2fe87a68e8df1b889
rodelcr <rodelcr@gmail.com>
2025-03-04 10:02:43 -0500

    New priors

--- ccl_hod_modeling.py
+++ ccl_hod_modeling.py
@@ -284,7 +284,9 @@ def log_prior(sample, hod_str = 'nicola20'):
         
     if hod_str=="nicola20" or hod_str=="zheng07":
         M_min, sig_logm, M_0, M_1, alpha = sample 
-        if 9.0 < M_min < 16.0 and 9.0 < M_0 < 16.0 and 0.0 < alpha < 2.0 and 0 < sig_logm < 2 and 10.0 < M_1 < 16.0 and (((M_0/M_min) -1) >= 0):
+        max_M = 16.95
+        min_M = 9.0
+        if min_M < M_min < max_M and min_M < M_0 < max_M and 0.0 < alpha < 2.0 and 0 < sig_logm < 2 and min_M < M_1 < max_M and (((M_0/M_min) -1) >= 0):
             return 0.0
         else:
             return -np.inf
